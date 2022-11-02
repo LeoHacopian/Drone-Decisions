7ccde6c0e7360c88f272dd98c6bd38a2d96c2dba
LeoHacopian <65243277+LeoHacopian@users.noreply.github.com>
2022-11-02 14:40:16 -0700

    photo randomized

--- src/experiment.js
+++ src/experiment.js
@@ -26,17 +26,27 @@ export async function run({ assetPaths, input = {}, environment, title, version 
 
   const timeline = [];
 
+  const test_stimuli = [
+    {stimulus: "assets/maps/ph1.png"},
+    {stimulus: "assets/maps/ph2.png"},
+    {stimulus: "assets/maps/ph3.png"},
+    {stimulus: "assets/maps/ph4.png"},
+    {stimulus: "assets/maps/ph5.png"},
+    {stimulus: "assets/maps/ph6.png"}
+  ]
+
+
   // Preload assets
   timeline.push({
     type: PreloadPlugin,
-    images: ['assets/imgs/drone.jpg', 'assets/imgs/no_drone.jpg']
+    images: assetPaths.images
   });
 
   // Welcome screen
-  timeline.push({
-    type: HtmlKeyboardResponsePlugin,
-    stimulus: "<p>Welcome to Drone Decisions!<p/>",
-  });
+  // timeline.push({
+  //   type: HtmlKeyboardResponsePlugin,
+  //   stimulus: "<p>Welcome to Drone Decisions!<p/>",
+  // });
 
   let buttons = ['<button class="jspsych-btn"><img src="assets/imgs/no_drone.jpg"></img></button>',
                  '<button class="jspsych-btn"><img src="assets/imgs/drone_attack.jpg"></img></button>']
@@ -46,19 +56,42 @@ export async function run({ assetPaths, input = {}, environment, title, version 
   let region = ['North America', 'South America', 'Europe', 'Africa', 'Central Asia', 'East Asia', 'Middle East'];
   let confidence = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95];
   let bystanders = Math.floor(Math.random() * 10);
-  //
-  timeline.push({
+  let regionMap = ["assets/maps/ph1.png", "assets/maps/ph2.png", "assets/maps/ph3.png", "assets/maps/ph4.png", "assets/maps/ph5.png", "assets/maps/ph6.png"]
+
+  console.log(jsPsych.timelineVariable("stimulus"))
+
+  const drone = {
     type: HtmlButtonResponsePlugin,
-    stimulus: `<div><img src="assets/imgs/drone.jpg"></img></div>
-    <p>Your target is located in a ${size[Math.floor(Math.random()*size.length)]} ${sizeRelation[Math.floor(Math.random()*sizeRelation.length)]} country in ${region[Math.floor(Math.random()*region.length)]}. There are ${bystanders} bystanders in the area and you are ${confidence[Math.floor(Math.random()*confidence.length)]}% confident that you have identified the correct target.</p>
-    `,
+    stimulus:
+        function (){
+          const text =`
+            <div><img src='${jsPsych.timelineVariable('stimulus')}'></img></div>
+            <p>Your target is located in a ${size[Math.floor(Math.random()*size.length)]} 
+                ${sizeRelation[Math.floor(Math.random()*sizeRelation.length)]} country in 
+                ${region[Math.floor(Math.random()*region.length)]}. There are 
+                ${bystanders} bystanders in the area and you are 
+                ${confidence[Math.floor(Math.random()*confidence.length)]}% confident that you have identified the correct target.</p>
+            `
+          return text
+        }
+,
     button_html: buttons,
     choices: ['Yes', 'No'],
-  });
+  };
+
+  const node = {
+    timeline: [drone],
+    timeline_variables: test_stimuli,
+    randomize_order: true,
+    repetitions: 5
+  };
+
+  timeline.push(node);
+
 
   await jsPsych.run(timeline);
 
   // Return the jsPsych instance so jsPsych Builder can access the experiment results (remove this
   // if you handle results yourself, be it here or in `on_finish()`)
-  return jsPsych.data.get().localSave('csv', 'Result.csv');
+  //return jsPsych.data.get().localSave('csv', 'Result.csv');
 }
