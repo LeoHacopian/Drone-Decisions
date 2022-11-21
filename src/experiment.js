ef9fd8bc279c6df62893bf2164b1ec819000cc02
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-11-21 13:31:13 -0800

    rename files

--- src/experiment.js
+++ src/experiment.js
@@ -29,7 +29,10 @@ const REGION = [
 const CONFIDENCE = [
   5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95,
 ];
-// const SIZE = ["small", "medium-sized", "large"]
+const MAP_TYPES = [
+  "City A/BCA", "City B/BCB", "City A/SCA", "City B/SCB", "A/Medium-Sized B", "A/Medium-Sized B"
+]
+const SIZE = ["small", "medium-sized", "large"]
 // const BYSTANDERS = [0, 10]
 
 /* GLOBAL FUNCTIONS */
